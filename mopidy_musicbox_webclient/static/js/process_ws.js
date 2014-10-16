b735de3f2bf0f0e76e45dd39e1735e6913482d4a
Loet Avramson <loet@forter.com>
2014-10-16 23:25:43 +0300

    Don't copy and paste blindly.

--- mopidy_musicbox_webclient/static/js/process_ws.js
+++ mopidy_musicbox_webclient/static/js/process_ws.js
@@ -105,7 +105,7 @@ function processBrowseDir(resultArr) {
         iconClass = getMediaClass(resultArr[i].uri);
 	if(resultArr[i].type == 'track' ) {
 //	    console.log(resultArr[i]);
-            child += '<li id="browselisttracks-' + resultArr[i].uri + '"><a href="#" class="moreBtn" onclick="return popupTracks(event, \'' + uri + '\',\'' + song.uri + '\');">' +
+            child += '<li id="browselisttracks-' + resultArr[i].uri + '"><a href="#" class="moreBtn" onclick="return popupTracks(event, \'' + undefined + '\',\'' + resultArr[i].uri + '\');">' +
                 '<a href="#" class="browsetrack" onclick="return playBrowsedTracks(0, this.id);" id="' + resultArr[i].uri + 
                 '"><h1 class="trackname"><i class="' + iconClass + '"></i> ' + resultArr[i].name + '</h1></a></li>';
 	} else {
